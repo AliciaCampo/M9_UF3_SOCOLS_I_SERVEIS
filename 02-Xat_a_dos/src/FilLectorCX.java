551a2d778a4e439ded500db41ddc2bf0b95a69c1
AliciaCampo <145765516+AliciaCampo@users.noreply.github.com>
2025-04-25 22:15:12 +0200

    Ejercicio terminado cierra bien ahora tanto CLient como Servidor con sortir

--- 02-Xat_a_dos/src/FilLectorCX.java
+++ 02-Xat_a_dos/src/FilLectorCX.java
@@ -1,22 +1,26 @@
+import java.io.EOFException;
 import java.io.ObjectInputStream;
-
 public class FilLectorCX extends Thread {
     private ObjectInputStream ois;
-
     public FilLectorCX(ObjectInputStream ois) {
         this.ois = ois;
     }
-
     @Override
     public void run() {
         try {
             String missatge;
-            while (!(missatge = (String) ois.readObject()).equals("sortir")) {
+            while (true) {
+                missatge = (String) ois.readObject();
+                // si el servidor decide enviar “sortir”:
+                if (missatge.equals("sortir")) break;
                 System.out.println("Rebut: " + missatge);
             }
-            System.out.println("Fil de lectura finalitzat.");
+        } catch (EOFException eof) {
+            // socket cerrado desde el otro extremo → fin de lectura
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            System.out.println("Fil de lectura finalitzat.");
         }
     }
 }
